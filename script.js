ba9f66a1f97a9440e490a69be2dedcfe8a463075
Erickthe <erickouellette1105@gmail.com>
2025-10-18 13:49:47 -0400

    Section Compétences achevée

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const menu = document.getElementById('menu');
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+
+function show() {
+  const show = document.getElementById("hidden");
+  show.style.display = "block"
+}
