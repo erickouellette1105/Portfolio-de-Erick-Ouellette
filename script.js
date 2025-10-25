11f61829e33c7d9be7183d9d42580964ce51db4d
Erickthe <erickouellette1105@gmail.com>
2025-10-25 17:09:21 -0400

    Début de la composante(le dernier commit était une erreur de compréhension)

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const menu = document.getElementById('menu');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
+  if (hamburger && menu) {
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  }
 });
 
 
